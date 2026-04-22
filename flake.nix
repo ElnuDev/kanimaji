cc21cbe405dfad0ed99b06ca6564d230f2826907
ElnuDev <elnu@elnu.com>
2026-04-22 14:28:38 -0700

    basic shell flake without svgexport support

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Beautiful animated SVG or GIF kanji from KanjiVG data set.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    kanijvg = {
+      url = "github:KanjiVG/kanjivg";
+      flake = false;
+    };
+  };
+
+  outputs = { nixpkgs, ... }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+    python = "python314";
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = [ pkgs."${python}" ] ++ (with pkgs."${python}Packages"; [
+        lxml
+        svg-path
+      ]);
+    };
+  };
+}
